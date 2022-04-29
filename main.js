cb8757e9b01b7dc0b8b1cb76d8c40a5abccab334
ClydeTech <49488090+ClydeTech@users.noreply.github.com>
2022-04-29 15:10:40 +0100

    Chore: Splitting out files
    Separating JS, CSS, stations list

---
+++ main.js
@@ -0,0 +1,33 @@
+async function RunCommand(event, command) {
+    var url = "http://192.168.1.77/httpapi.asp?command=" + (command ?? event.getAttribute('data-command'))
+    return await fetch(url, {
+        method: 'GET'
+    })
+}
+async function main() {
+    try {
+        var response = await RunCommand(null, 'getPlayerStatus')
+        var status = await response.json()
+    } catch {
+        var status = { vol: 20 }
+    }
+
+    $("#volumeBar").slider({
+        min: 0,
+        max: 100,
+        value: status.vol,
+        range: "min",
+        slide: function (event, ui) {
+            RunCommand(event, "setPlayerCmd:vol:" + ui.value)
+        }
+    })
+
+    var listItems = []
+
+    $(stations).each(function (i, item) {
+        listItems.push('<div class="station button buttonGreen" onclick="RunCommand(this)" data-command="setPlayerCmd:m3u:play:' + item.Url + '">' + item.Title + '</div>')
+    })
+
+    $('.stations-container').append(listItems.join(''))
+}
+main()
---
+++ stations.js
@@ -0,0 +1,58 @@
+var stations = [
+    {
+        "Title": "BBC Radio 1",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_med/ak/bbc_radio_one.m3u8"
+    },
+    {
+        "Title": "BBC Radio 1Xtra",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_med/ak/bbc_1xtra.m3u8"
+    },
+    {
+        "Title": "BBC Radio 1Dance",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_med/ak/bbc_radio_one_dance.m3u8"
+    },
+    {
+        "Title": "BBC Radio 1Relax",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_med/ak/bbc_radio_one_relax.m3u8"
+    },
+    {
+        "Title": "BBC Radio 2",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_med/ak/bbc_radio_two.m3u8"
+    },
+    {
+        "Title": "BBC Radio 3",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_med/ak/bbc_radio_three.m3u8"
+    },
+    {
+        "Title": "BBC Radio 4",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_med/ak/bbc_radio_fourfm.m3u8"
+    },
+    {
+        "Title": "BBC Radio 4 LW",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_med/ak/bbc_radio_fourlw.m3u8"
+    },
+    {
+        "Title": "BBC Radio 4 Extra",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_med/ak/bbc_radio_four_extra.m3u8"
+    },
+    {
+        "Title": "BBC Radio 5 live",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_med/ak/bbc_radio_five_live.m3u8"
+    },
+    {
+        "Title": "BBC Radio 6 Music",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_6music.m3u8"
+    },
+    {
+        "Title": "BBC Radio 5 Live sports extra",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_med/ak/bbc_radio_five_live_sports_extra.m3u8"
+    },
+    {
+        "Title": "BBC Radio Scotland FM",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/ak/bbc_radio_scotland_fm.m3u8"
+    },
+    {
+        "Title": "BBC Radio Scotland MW",
+        "Url": "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_med/ak/bbc_radio_scotland_mw.m3u8"
+    }
+]
